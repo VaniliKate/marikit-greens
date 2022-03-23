fcd853aff76917b4d99480da437ad2d70dd03d31
Alex Kiborgok <borgokalek14@gmail.com>
2022-03-23 12:48:36 +0300

    add function to add a single category

--- admin/script.js
+++ admin/script.js
@@ -1,4 +1,5 @@
 $(document).ready(function () {
+  //get categories
   function getCategories() {
     if (localStorage.getItem("categories")) {
       console.log("in run");
@@ -8,24 +9,53 @@ $(document).ready(function () {
     let categories = JSON.parse(localStorage.getItem("categories"));
     return categories;
   }
+//Add category
+  function addCategory(gategoryName) {
+    if (localStorage.getItem("categories")) {
+      console.log("in run");
+      let categories = JSON.parse(localStorage.getItem("categories"));
+      let itemId = categories.length + 1;
+      let item = { id: itemId, name: gategoryName };
+      localStorage.setItem("categories", JSON.stringify([...categories, item]));
+      return item;
+    }
+    console.log("out run");
+    localStorage.setItem("categories", JSON.stringify([]));
+    let categories = JSON.parse(localStorage.getItem("categories"));
+    let itemId = categories.length + 1;
+    let item = { id: itemId, name: gategoryName };
+    localStorage.setItem("categories", JSON.stringify([...categories, item]));
+    return item;
+  }
+
   let categories = getCategories();
-  categories.forEach((element) => {
+    console.log(categories);
+    //Append categories to table
+  if (categories === null) {
     $(".category-list").append(
-      `<tr>
+      `<tr id="no-cat">
+        
+        <td>There are no categories<td>
+    </tr>`
+    );
+  } else {
+    categories.forEach((element) => {
+      $(".category-list").append(
+        `<tr>
         <th scope="row">${element.id}</th>
         <td>${element.name}</td>
     </tr>`
-    );
+      );
+    });
+  }
+  //Append one category to table on add
+  $(".add-category").click(function () {
+    let categoryName = $("#category-name").val();
+      let category = addCategory(categoryName);
+      $("#no-cat").remove()
+    $(".category-list").append(`<tr>
+                <th scope="row">${category.id}</th>
+                <td>${category.name}</td>
+              </tr>`);
   });
-
-  // $(".add-category").click(function () {
-  //     let categoryName = $("#category-name").val()
-  //     let categories = addItem(categoryName)
-  //     console.log(categories)
-
-  //     $(".category-list").append(`<tr>
-  //               <th scope="row">${categoryId}</th>
-  //               <td>${categoryName}</td>
-  //             </tr>`);
-  // })
 });
