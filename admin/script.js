0b7f9063f3d79104b79f27fd252a2d3e58e9a468
Alex Kiborgok <borgokalek14@gmail.com>
2022-03-23 12:23:26 +0300

    add getCategories function

--- admin/script.js
+++ admin/script.js
@@ -1,1 +1,31 @@
-$
+$(document).ready(function () {
+  function getCategories() {
+    if (localStorage.getItem("categories")) {
+      console.log("in run");
+      let categories = JSON.parse(localStorage.getItem("categories"));
+      return categories;
+    }
+    let categories = JSON.parse(localStorage.getItem("categories"));
+    return categories;
+  }
+  let categories = getCategories();
+  categories.forEach((element) => {
+    $(".category-list").append(
+      `<tr>
+        <th scope="row">${element.id}</th>
+        <td>${element.name}</td>
+    </tr>`
+    );
+  });
+
+  // $(".add-category").click(function () {
+  //     let categoryName = $("#category-name").val()
+  //     let categories = addItem(categoryName)
+  //     console.log(categories)
+
+  //     $(".category-list").append(`<tr>
+  //               <th scope="row">${categoryId}</th>
+  //               <td>${categoryName}</td>
+  //             </tr>`);
+  // })
+});
