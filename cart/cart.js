0a0f7057236d68af2b4cf05b87632e921a29dd22
Moringa School <wangu.mwangi@student.moringaschool.com>
2022-03-23 22:46:57 +0300

    fix:fixed a bug in cart html file

--- cart/cart.js
+++ cart/cart.js
@@ -116,3 +116,186 @@ $(".cboxseven").hover(
     });
   }
 );
+// boxtwo
+
+$(".cboxeight").hover(
+  function () {
+    $(".ccovereight").show(function () {
+      $(".cboxeight").animate({ height: "35vh", width: "25vw" });
+      $(".ccovereight").animate({ height: "35vh", width: "25vw" });
+    });
+  },
+  function () {
+    $(".ccovereight").hide(function () {
+      $(".cboxeight").animate({ height: "30vh", width: "20vw" });
+      $(".ccovereight").animate({ height: "30vh", width: "20vw" });
+    });
+  }
+);
+
+// boxthree
+
+$(".cboxnine").hover(
+  function () {
+    $(".ccovernine").show(function () {
+      $(".cboxnine").animate({
+        height: "35vh",
+        width: "25vw",
+      });
+      $(".ccovernine").animate({ height: "35vh", width: "25vw" });
+    });
+  },
+  function () {
+    $(".ccovernine").hide(function () {
+      $(".cboxnine").animate({ height: "30vh", width: "20vw" });
+      $(".ccovernine").animate({ height: "30vh", width: "20vw" });
+    });
+  }
+);
+//boxfour
+
+$(".cboxten").hover(
+  function () {
+    $(".ccoverten").show(function () {
+      $(".cboxten").animate({ height: "35vh", width: "25vw" });
+      $(".ccoverten").animate({ height: "35vh", width: "25vw" });
+    });
+  },
+  function () {
+    $(".ccoverten").hide(function () {
+      $(".cboxten").animate({ height: "30vh", width: "20vw" });
+      $(".ccoverten").animate({ height: "30vh", width: "20vw" });
+    });
+  }
+);
+
+$(".cboxeleven").hover(
+  function () {
+    $(".ccovereleven").show(function () {
+      $(".cboxeleven").animate({ height: "35vh", width: "25vw" });
+      $(".ccovereleven").animate({ height: "35vh", width: "25vw" });
+    });
+  },
+  function () {
+    $(".ccovereleven").hide(function () {
+      $(".cboxeleven").animate({ height: "30vh", width: "20vw" });
+      $(".ccovereleven").animate({ height: "30vh", width: "20vw" });
+    });
+  }
+);
+
+$(".cboxtwelve").hover(
+  function () {
+    $(".ccovertwelve").show(function () {
+      $(".cboxtwelve").animate({ height: "35vh", width: "25vw" });
+      $(".ccovertwelve").animate({ height: "35vh", width: "25vw" });
+    });
+  },
+  function () {
+    $(".ccovertwelve").hide(function () {
+      $(".cboxtwelve").animate({ height: "30vh", width: "20vw" });
+      $(".ccovertwelve").animate({ height: "30vh", width: "20vw" });
+    });
+  }
+);
+
+//   cfour
+$(".cbox13").hover(
+  function () {
+    $(".ccover13").show(function () {
+      $(".cbox13").animate({ height: "30vh", width: "20vw" });
+      $(".ccover13").animate({ height: "30vh", width: "20vw" });
+    });
+  },
+  function () {
+    $(".ccover13").hide(function () {
+      $(".cbox13").animate({ height: "25vh", width: "15vw" });
+      $(".ccover13").animate({ height: "25vh", width: "15vw" });
+    });
+  }
+);
+$(".cbox14").hover(
+  function () {
+    $(".ccover14").show(function () {
+      $(".cbox14").animate({ height: "30vh", width: "20vw" });
+      $(".ccover14").animate({ height: "30vh", width: "20vw" });
+    });
+  },
+  function () {
+    $(".ccover14").hide(function () {
+      $(".cbox14").animate({ height: "25vh", width: "15vw" });
+      $(".ccover14").animate({ height: "25vh", width: "15vw" });
+    });
+  }
+);
+$(".cbox15").hover(
+  function () {
+    $(".ccover15").show(function () {
+      $(".cbox15").animate({ height: "30vh", width: "20vw" });
+      $(".ccover15").animate({ height: "30vh", width: "20vw" });
+    });
+  },
+  function () {
+    $(".ccover15").hide(function () {
+      $(".cbox15").animate({ height: "25vh", width: "15vw" });
+      $(".ccover15").animate({ height: "25vh", width: "15vw" });
+    });
+  }
+);
+// container6
+$(".cbox16").hover(
+  function () {
+    $(".ccover16").show(function () {
+      $(".cbox16").animate({ height: "30vh", width: "20vw" });
+      $(".ccover16").animate({ height: "30vh", width: "20vw" });
+    });
+  },
+  function () {
+    $(".ccover16").hide(function () {
+      $(".cbox16").animate({ height: "25vh", width: "15vw" });
+      $(".ccover16").animate({ height: "25vh", width: "15vw" });
+    });
+  }
+);
+$(".cbox17").hover(
+  function () {
+    $(".ccover17").show(function () {
+      $(".cbox17").animate({ height: "30vh", width: "20vw" });
+      $(".ccover17").animate({ height: "30vh", width: "20vw" });
+    });
+  },
+  function () {
+    $(".ccover17").hide(function () {
+      $(".cbox17").animate({ height: "25vh", width: "15vw" });
+      $(".ccover17").animate({ height: "25vh", width: "15vw" });
+    });
+  }
+);
+$(".cbox18").hover(
+  function () {
+    $(".ccover18").show(function () {
+      $(".cbox18").animate({ height: "30vh", width: "20vw" });
+      $(".ccover18").animate({ height: "30vh", width: "20vw" });
+    });
+  },
+  function () {
+    $(".ccover18").hide(function () {
+      $(".cbox18").animate({ height: "25vh", width: "15vw" });
+      $(".ccover18").animate({ height: "25vh", width: "15vw" });
+    });
+  }
+);
+
+$("#product").click(function () {
+  let name = $(this).find("h6").text();
+  let price = $(this).find("p").text();
+  let newItem = `
+      <div id="newItem">
+        <h4>${name}</h4>
+        <p>${price}</p>
+        <button id="remove">Remove</button>
+      </div>
+      `;
+
+  $("#cart-items").append(newItem);
+});
