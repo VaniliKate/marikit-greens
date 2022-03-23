17f5df437372b1ebbe4f4e33c93ed94370d4bbf5
Vanili <katevanili98@gmail.com>
2022-03-23 23:00:00 +0300

    Change to the signup validation section

--- js/signup-login.js
+++ js/signup-login.js
@@ -21,98 +21,54 @@ function store(){
     var numbers = /[0-9]/g;
 
     if(name.value.length == 0){
-        alert('Please fill in email');
+        alert('Please fill in your name');
 
     }else if(pw.value.length == 0){
         alert('Please fill in password');
 
     }else if(email.value.length == 0){
-        alert('Please fill in email');
+        alert('Please fill in your email');
 
-    }else if(name.value.length == 0 && pw.value.length == 0 && email.value.lenth == 0){
-        alert('Please fill in name, password and email');
+    }else if(name.value.length == 0 && pw.value.length == 0){
+        alert('Please fill in email and password');
 
     }else if(pw.value.length > 24){
         alert('Max of 24');
 
     }else if(!pw.value.match(numbers)){
-        alert('please add 1 number to your password');
+        alert('please add 1 number');
 
     }else if(!pw.value.match(upperCaseLetters)){
-        alert('please add 1 uppercase letter to your password');
+        alert('please add 1 uppercase letter');
 
     }else if(!pw.value.match(lowerCaseLetters)){
-        alert('please add 1 lowercase letter to your password');
+        alert('please add 1 lovercase letter');
 
     }else{
-        JSON.stringify(localStorage.setItem('name', name.value));
-        JSON.stringify(localStorage.setItem('email', email.value));
-        JSON.stringify(localStorage.setItem('pw', pw.value));
+        localStorage.setItem('name', name.value);
+        localStorage.setItem('pw', pw.value);
+        localStorage.setItem('email', email.value);
         alert('Your account has been created');
-        window.location("../test-login.html");
     }
 }
 
 //checking
 function check(){
-    var storedName = JSON.parse(localStorage.getItem('name'));
-    var storedPw = JSON.parse(localStorage.getItem('pw'));
-    
+    var storedName = localStorage.getItem('email');
+    var storedPw = localStorage.getItem('pw');
+
     var userName = document.getElementById('name');
     var userPw = document.getElementById('pass');
     var userRemember = document.getElementById("rememberMe");
 
     if(userName.value == storedName && userPw.value == storedPw){
         alert('You are logged in.');
+        window.location.href = "https://www.tutorialrepublic.com/";
     }else{
         alert('Error on login');
     }
 }
 
-
-//google sign in
-$('#google-btn').on('click', function() {
-	// Initialize with your OAuth.io app public key
-	OAuth.initialize('txp2rKYpuKZXaaYC5kB-m13KnVE');
-  OAuth.popup('google').then(google => {
-    console.log('google:',google);
-    // Retrieves user data from oauth provider
-    // Prompts 'welcome' message with User's email on successful login
-    // #me() is a convenient method to retrieve user data without requiring you
-    // to know which OAuth provider url to call
-    google.me().then(data => {
-      console.log('me data:', data);
-      alert('Google says your email is:' + data.email + ".\nView browser 'Console Log' for more details");
-	  });
-    // Retrieves user data from OAuth provider by using #get() and
-    // OAuth provider url
-    google.get('https://people.googleapis.com/v1/people/me?personFields=names,emailAddresses,occupations,organizations,addresses,locales').then(data => {
-      console.log('self data:', data);
-    })
-	});
-})
-
-//facebook sign in
-$('#fcbk-btn').on('click', function() {
-	// Initialize with your OAuth.io app public key
-	OAuth.initialize('HwAr2OtSxRgEEnO2-JnYjsuA3tc');
-  // Use popup for oauth
-  OAuth.popup('facebook').then(facebook => {
-    console.log('facebook:',facebook);
-    // Prompts 'welcome' message with User's email on successful login
-    // #me() is a convenient method to retrieve user data without requiring you
-    // to know which OAuth provider url to call
-    facebook.me().then(data => {
-      console.log('me data:', data);
-      alert('Facebook says your email is:' + data.email + ".\nView browser 'Console Log' for more details");
-	  })
-    // Retrieves user data from OAuth provider by using #get() and
-    // OAuth provider url
-    facebook.get('/v2.5/me?fields=name,first_name,last_name,email,gender,location,locale,work,languages,birthday,relationship_status,hometown,picture').then(data => {
-      console.log('self data:', data);
-      })
-  });
-})
 
 /*Sign up form validation
 $(document).ready(function () {
