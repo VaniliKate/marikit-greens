6899fdedce336312bc3ae858c08c9ec5f16c876c
Annetkhavere <anneywan5@gmail.com>
2022-03-23 23:02:12 +0300

    add background image

---
+++ delivery/delivery.js
@@ -0,0 +1,52 @@
+$(document).ready(function(){
+
+  $("#deliver").submit(function(event){
+    event.preventDefault();
+    getItDelivered();
+});
+  
+  $(".form").submit(function(){
+    deliveryConfirmation();
+  });
+  
+  $("#pickup").submit(function(){
+    pickUp();
+  });
+});
+
+function getItDelivered() {
+    $(".deliversection").show();
+    $("#pickup").hide();
+    $("#deliver").hide();
+    $("#deliveryoptions").hide();
+
+  }
+
+function deliveryConfirmation() {
+  //get values from the form
+
+  $("#pickup").hide();
+  $("#deliver").hide();
+  $("#deliveryoptions").hide();
+  $(".deliversection").hide();
+
+  let firstName = $("#firstname").val();
+  let lastName = $("#lastname").val();
+  let phoneNumber = $("#phonenumber").val();
+  let location = $("#location").val();
+ 
+  $(".delivery-feedback-image").show();
+  $(".delivery-feedback").text("Thank you " + firstName + " " + lastName + " for shopping with Marikiti Greens. Your greens will be delivered to " + location + ". For any further communication, we shall reach you through this number " + "+254787878787");
+  $(".delivery-feedback").show();
+
+}
+
+function pickUp() {
+  $("#pickup").hide();
+  $("#deliver").hide();
+  $("#deliveryoptions").hide();
+  $(".deliversection").hide();
+
+  $(".pickup-feedback").show();
+}
+
