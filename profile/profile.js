db1616ce93cdac3430bf6b78cc2598c0ee416f36
Wamaitha <wamaithaweru19@gmail.com>
2022-03-24 10:14:54 +0300

    edit styles

--- profile/profile.js
+++ profile/profile.js
@@ -1,5 +1,16 @@
+// get data from local storage
 
+function myProfile() {
+  var profileName = localStorage.getItem("name");
+  let profileEmail = localStorage.getItem("email");
+  profileName = JSON.parse(profileName);
 
+  profileEmail = JSON.parse(profileEmail);
+}
+$(document).ready(function () {});
 
-
-
+// getData = ()=> {
+//     let data = local.getItem('user-info');
+//     // get it as object
+//     data = JSON.parse(data);
+// document.write
