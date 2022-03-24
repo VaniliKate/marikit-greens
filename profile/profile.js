9905baa4193a813bf486e0c30c9f626ce83e2197
Wamaitha <wamaithaweru19@gmail.com>
2022-03-24 11:02:17 +0300

    edit profile folder

--- profile/profile.js
+++ profile/profile.js
@@ -1,5 +1,13 @@
+function myProfile(){
+    var name = localStorage.getItem('name');
+    name= JSON.parse(name);
+    var email = localStorage.getItem('email');
+    email= JSON.parse(email);
+}
+ 
+$(document).ready(function(){
 
-
+});
 
 
 
