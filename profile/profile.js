5a361818e693bd82826b7b638076cb9608e1f5cc
Wamaitha <wamaithaweru19@gmail.com>
2022-03-24 12:56:58 +0300

    edit profile.css

--- profile/profile.js
+++ profile/profile.js
@@ -1,10 +1,12 @@
 function myProfile(){
     var profileName = localStorage.getItem('name'); 
     var profileEmail= localStorage.getItem('email');
+ 
+
 
     event.preventDefault();
-    document.getElementsByClassName("nametext").innerHTML = 'name';
-    document.getElementsByClassName("emailtext").innerHTML = 'email';
+    // document.getElementsByClassName("nametext").innerHTML = 'name';
+    // document.getElementsByClassName("emailtext").innerHTML = 'email';
     
 }
 
