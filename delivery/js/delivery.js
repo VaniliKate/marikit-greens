6eba310f6a4f9f6a12c49252572a8c6f087a6ecc
Annetkhavere <anneywan5@gmail.com>
2022-03-24 09:00:24 +0300

    add alerts to the form

--- delivery/js/delivery.js
+++ delivery/js/delivery.js
@@ -6,41 +6,56 @@ $(document).ready(function(){
 });
   
   $(".form").submit(function(){
-    deliveryConfirmation();
+    formInputs();
   });
-  
+
   $("#pickup").submit(function(){
     pickUp();
   });
 });
 
 function getItDelivered() {
-    $(".deliversection").show();
-    $("#pickup").hide();
-    $("#deliver").hide();
-    $("#deliveryoptions").hide();
-    $(".options").hide();
+  $(".deliversection").show();
+  $("#pickup").hide();
+  $("#deliver").hide();
+  $("#deliveryoptions").hide();
+  $(".options").hide();
+  
+}
 
-  }
 
-function deliveryConfirmation() {
-  //get values from the form
+function formInputs() {
+
+let firstName = $("#firstname").val();
+let lastName = $("#lastname").val();
+let phoneNumber = $("#phonenumber").val();
+let location = $("#location").val();
+
+  if (firstName === "") {
+    alert("Please enter your first name")
+  } else if(lastName === "") {
+    alert("Please enter your last name")
+  } else if(phoneNumber === "") {
+    alert("Please enter your phone number")
+  } else if(location === "") {
+    alert ("Please enter your location")
+  } else if (firstName !== "" && lastName !== "" && phoneNumber !== "" && location !== "") {
 
   $("#pickup").hide();
   $("#deliver").hide();
   $("#deliveryoptions").hide();
   $(".deliversection").hide();
 
-  let firstName = $("#firstname").val();
-  let lastName = $("#lastname").val();
-  let phoneNumber = $("#phonenumber").val();
-  let location = $("#location").val();
- 
   $(".delivery-feedback-image").show();
   $(".delivery-feedback").text("Thank you " + firstName + " " + lastName + " for shopping with Marikiti Greens. Your greens will be delivered to " + location + ". For any further communication, we shall reach you through this number " + "+254787878787");
   $(".delivery-feedback").show();
 
+  }
+
 }
+
+
+
 
 function pickUp() {
   $(".options").hide();
